1ea0a94460d24fd2c093df5686c75ac9fccbfada
Eduardo Leite <leite.eduardo@academico.ifpb.edu.br>
2022-02-20 00:04:55 -0300

    first commit

---
+++ _javascript/funcoes.js
@@ -0,0 +1,27 @@
+function mudaFoto (foto)
+{
+    document.getElementById("icone").src = foto;
+}
+
+ function validaForm()
+ {
+    erro = false;
+    if($('#nome').val() == '')
+    {
+        alert('Você precisa preencher o campo Nome');erro = true;
+    }
+    if($('#email').val() == '' && !erro)
+    {
+        alert('Você precisa preencher o campo E-mail');erro = true;
+    }
+    if($('#mensagem').val() == '' && !erro)
+    {
+        alert('Você precisa preencher o campo Mensagem');erro = true;
+    }
+
+    //se nao tiver erros
+    if(!erro)
+    {
+        $('#formulario_contato').submit();
+    }
+ }
